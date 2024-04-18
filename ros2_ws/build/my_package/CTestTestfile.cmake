81898c5c79c0dc08c508a0a91a914ab85e065213
Lazar Milic <lazarmilic2@gmail.com>
2024-04-18 20:42:11 +0200

    Clean

--- ros2_ws/build/my_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dexter/Programming/ros2_workspaces/Industrijska-Robotika-ROS2/ros2_ws/src/my_package
-# Build directory: /home/dexter/Programming/ros2_workspaces/Industrijska-Robotika-ROS2/ros2_ws/build/my_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
